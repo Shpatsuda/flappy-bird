fd0164d35df58fa7b639b422fc27bf3d32f4c855
Humperdoo <manzosaw@gmail.com>
2023-09-02 19:03:03 +0300

    spawner + minor game tweaks

---
+++ Flappy/Assets/Scripts/spawner.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject pipePrefab;
+    public float spawnInterval = 1f;
+    public float minHeight = -1f;
+    public float maxHeight = 1f;
+
+    private void Start()
+    {
+
+        InvokeRepeating(nameof(Spawn), spawnInterval, spawnInterval);
+
+    }
+    private void Spawn()
+    {
+
+        GameObject pipes = Instantiate(pipePrefab, transform.position, Quaternion.identity);
+        pipes.transform.position += Vector3.up * Random.Range(minHeight, maxHeight);
+
+    }
+
+
+}
